864ea95d1778ca58125e94398753517306357a32
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-26 20:21:22 +0200

    Impelement Producer Repository

---
+++ eTTickets.Infrasturcture/Repositories/ProducerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain.Repositories;
+using eTickets.Domain.Models;
+using eTickets.Infrasturcture.Data;
+
+namespace eTickets.Infrasturcture.Repositories
+{
+	public class ProducerRepository : GenericRepository<Producer>, IProducerRepository
+	{
+		public ProducerRepository(AppDbContext _context) : base(_context)
+		{
+		}
+	}
+}
