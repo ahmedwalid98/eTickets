2842b00c4c14959c9cd4300a7aa08927339bb049
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-26 20:14:57 +0200

    Impelemnt the movie repo

---
+++ eTTickets.Infrasturcture/Repositories/MovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain.Repositories;
+using eTickets.Domain.Entities;
+using eTickets.Infrasturcture.Data;
+
+namespace eTickets.Infrasturcture.Repositories
+{
+	public class MovieRepository : GenericRepository<Movie>, IMovieRepository
+	{
+		public MovieRepository(AppDbContext _context) : base(_context)
+		{
+		}
+	}
+}
