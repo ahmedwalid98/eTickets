de8f7a890fe89a75b4df427cff304483bfa34da6
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-26 19:42:56 +0200

    Implemented Basics Models

---
+++ eTicket.Domain/Entities/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain.Entities;
+using eTickets.Domain.Entities;
+
+namespace eTickets.Domain.Models
+{
+	public class Actor: BaseEntity
+	{
+        public string ProfilePictureUrl { get; set; } = string.Empty;
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+		public ICollection<MovieActor> MoviesActors { get; set; }
+	}
+}
---
+++ eTicket.Domain/Entities/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain.Entities;
+using eTickets.Domain.Enums;
+using eTickets.Domain.Models;
+
+namespace eTickets.Domain.Entities
+{
+	public class Movie : BaseEntity
+	{
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+        [ForeignKey(nameof(Cinema))]
+        public int CinemaId { get; set; }
+        public Cinema Cinema { get; set; }
+        [ForeignKey(nameof(Producer))]
+        public int ProducerId { get; set; }
+        public Producer Producer { get; set; }
+
+        public ICollection<MovieActor> MoviesActors { get; set; }
+    }
+}
