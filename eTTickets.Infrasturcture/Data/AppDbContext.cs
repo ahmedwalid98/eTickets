e0a0e7d8b4a74f602e1b898ad6622ec771392d02
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-26 19:46:56 +0200

    Set up the infr enviroment

---
+++ eTTickets.Infrasturcture/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using eTickets.Domain.Entities;
+using eTickets.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Infrasturcture.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<MovieActor> MoviesActors { get; set; }
+
+    }
+}
---
+++ eTTickets.Infrasturcture/Data/Configurations/MovieActorConfigure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTickets.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eTickets.Infrasturcture.Data.Configurations
+{
+    public class MovieActorConfigure : IEntityTypeConfiguration<MovieActor>
+    {
+        public void Configure(EntityTypeBuilder<MovieActor> builder)
+        {
+            builder.HasKey(ma => new { ma.MovieId, ma.ActorId });
+        }
+    }
+}
