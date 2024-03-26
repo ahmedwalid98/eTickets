0a706b16ddd86faf99d2c5557a7801fcbf27133a
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-26 20:21:13 +0200

    Impelement CinemaRepository

---
+++ eTTickets.Infrasturcture/Repositories/CinemaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain.Repositories;
+using eTickets.Domain.Models;
+using eTickets.Infrasturcture.Data;
+
+namespace eTickets.Infrasturcture.Repositories
+{
+	public class CinemaRepository : GenericRepository<Cinema>, ICinemaRepository
+	{
+		public CinemaRepository(AppDbContext _context) : base(_context)
+		{
+		}
+	}
+}
