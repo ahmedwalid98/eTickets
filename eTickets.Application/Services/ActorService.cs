450daee853852ddeb5d4b646e5b231bbc9a40bb4
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-27 02:59:49 +0200

    Impelemented the actor service

---
+++ eTickets.Application/Services/ActorService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain;
+using eTicket.Domain.Entities;
+using eTicket.Domain.Services;
+
+namespace eTickets.Application.Services
+{
+    public class ActorService : IActorService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ActorService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Actor>> GetAllActors()
+        {
+            return await _unitOfWork.ActorRepository.GetAll();
+        }
+    }
+}
