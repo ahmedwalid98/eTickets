80bd981af580c98288ea90246caf9d4f00a8f3ca
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-28 20:48:30 +0200

    Implemented the method to get all Producers

---
+++ eTickets.Application/Services/ProducerService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain;
+using eTicket.Domain.Entities;
+using eTicket.Domain.Services;
+
+namespace eTickets.Application.Services
+{
+    public class ProducerService : IProducerService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProducerService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Producer>> GetAllProducers()
+        {
+            return await _unitOfWork.ProducerRepository.GetAll();
+        }
+    }
+}
