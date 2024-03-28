5767784099f3b85d5112ffb3604e3cbf42b5a85a
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-28 21:00:18 +0200

    Created the Controller and modified the index action

---
+++ eTicketsUI/Controllers/CinemaController.cs
@@ -0,0 +1,21 @@
+﻿using eTicket.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.UI.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly ICinemaService _cinemaService;
+
+        public CinemaController(ICinemaService cinemaService)
+        {
+            _cinemaService = cinemaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cinemas = await _cinemaService.GetAllCinemas();
+            return View(cinemas);
+        }
+    }
+}
