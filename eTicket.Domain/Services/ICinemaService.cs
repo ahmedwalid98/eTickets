65d3dbdaa462513d5b614933a217fc609175c07a
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-28 20:59:37 +0200

    Added method to get all cinemas

---
+++ eTicket.Domain/Services/ICinemaService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain.Entities;
+
+namespace eTicket.Domain.Services
+{
+    public interface ICinemaService
+    {
+        Task<IEnumerable<Cinema>> GetAllCinemas();
+    }
+}
