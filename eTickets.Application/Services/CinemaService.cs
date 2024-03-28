31d7bcf5a2b9c627692a23ab14f217b9bc8a1381
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-03-28 20:59:56 +0200

    Implemented the GetAllCinemas method logic

---
+++ eTickets.Application/Services/CinemaService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain;
+using eTicket.Domain.Entities;
+using eTicket.Domain.Services;
+
+namespace eTickets.Application.Services
+{
+    public class CinemaService : ICinemaService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CinemaService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Cinema>> GetAllCinemas()
+        {
+            return await _unitOfWork.CinemaRepository.GetAll();
+        }
+    }
+}
