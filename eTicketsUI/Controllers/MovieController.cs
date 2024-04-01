76819709f65b891ab1cc8259a4027bcb2f0286bc
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-04-01 02:36:58 +0200

    Created the controller and the index view of with with view model

---
+++ eTicketsUI/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using eTicket.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.UI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieService _movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _movieService.GetAllMovies();
+            return View(movies);
+        }
+    }
+}
---
+++ eTicketsUI/ViewModels/GetMovies.cs
@@ -0,0 +1,15 @@
+﻿namespace eTicketsUI.ViewModels
+{
+      public class GetMovies
+      {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public  string CinemaName { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+      }   
+}
