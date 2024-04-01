f91d9821e7ca3d70469c95cf4d47646a29a44096
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-04-01 02:35:37 +0200

    GaetAllMovies method to retrieve all movies

---
+++ eTicket.Domain/Services/IMovieService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eTicket.Domain.Entities;
+using eTicketsUI.ViewModels;
+
+
+namespace eTicket.Domain.Services
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<GetMovies>> GetAllMovies();
+    }
+}
