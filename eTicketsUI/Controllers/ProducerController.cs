027aac55ccd4908bb873a1348745eed3ce47051a
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-04-02 00:42:02 +0200

    Create detail actionb and detail views

--- eTicketsUI/Controllers/ProducerController.cs
+++ eTicketsUI/Controllers/ProducerController.cs
@@ -17,5 +17,10 @@ namespace eTickets.UI.Controllers
             var producers = await _producerService.GetAllProducers(); 
             return View(producers);
         }
+        public async Task<IActionResult> Detail(int id)
+        {
+            var producer = await _producerService.GetProducerById(id);
+            return View(producer);
+        }
     }
 }
