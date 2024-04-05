3f6e6575ff42524edcfcd694112b44b3c9a0addf
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-04-05 22:10:28 +0200

    removing validation from the entity

--- eTicket.Domain/Entities/Actor.cs
+++ eTicket.Domain/Entities/Actor.cs
@@ -10,13 +10,11 @@ namespace eTicket.Domain.Entities
 {
 	public class Actor: BaseEntity
 	{
-		[Required]
+		
         public string ProfilePictureUrl { get; set; } = string.Empty;
-		[Required]
-		[MinLength(3)]
-		[MaxLength(50)]
+		
 		public string FullName { get; set; }
-		[Required]
+		
         public string Bio { get; set; }
 		public ICollection<MovieActor> MoviesActors { get; set; }
 	}
