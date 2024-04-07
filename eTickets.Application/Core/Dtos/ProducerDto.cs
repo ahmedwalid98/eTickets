d78874a4de05ede0fa80dd786cf01ca9e1f7b38f
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2024-04-07 22:57:43 +0200

    Create the Dto and the req schema for producer

---
+++ eTickets.Application/Core/Dtos/ProducerDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Application.Core.Dtos
+{
+    public class ProducerDto
+    {
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+        public List<string> MoviesName { get; set; }
+    }
+}
---
+++ eTickets.Application/Core/Request/ProducerReq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Application.Core.Request
+{
+    public class ProducerReq
+    {
+        [Required(ErrorMessage = "Profile Picutre is required")]
+        public string ProfilePictureUrl { get; set; }
+        [Required(ErrorMessage = "Producer Name is required")]
+        [MinLength(3, ErrorMessage = "Producer Name Must be more than 3 chars")]
+        [MaxLength(50, ErrorMessage = "Producer name must be less than 50 chars")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Producer bio is required")]
+        public string Bio { get; set; }
+    }
+}
